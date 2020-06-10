a7a947691bac0eeefa652cd8170409f2862742cf
Kyle Nugent <kln5656@gmail.com>
2020-06-10 21:35:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,2 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-addElementToBeginningOfArray = chocolateBars.push('recee\'s')
+addElementToBeginningOfArray = chocolateBars.unshift('recee\'s')
