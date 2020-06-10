4377c32bce50290445836a7c03052cf24d11e3ce
Kyle Nugent <kln5656@gmail.com>
2020-06-10 22:13:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray() {
-  chocolateBars = ["recees", ...chocolateBars]
+  chocolateBars = ["foo", 1,  ...chocolateBars]
   console.log(chocolateBars)
   return chocolateBars
 }
